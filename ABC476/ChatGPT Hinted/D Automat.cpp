c21cb357d90195717e9f3d6f2248ff570f8a73c4
liyunyangdashen <tcl52bb@gmail.com>
2026-09-20 14:23:22 +0800

    Refactor D Automat.cpp for improved logic and variables

--- ABC476/ChatGPT Hinted/D Automat.cpp
+++ ABC476/ChatGPT Hinted/D Automat.cpp
@@ -1,23 +1,31 @@
 #include<iostream>
-#include<cmath>
-using namespace std; using ll = long long; const int N = 1505; 
-ll n,m,a[N],b[N],t[N][N],c[N][N],ans = 0;
+#include<algorithm>
+using namespace std; using ll = long long; const int N = 2e5 + 5; 
+ll n,m,k,x,y,a[N],b[N],pa[N],pb[N],nd[N]; int ans = 0;
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  cin >> n >> m;
+  cin >> n >> m >> k >> x >> y;
   for (int i = 1;i <= n;i++)
     cin >> a[i];
-  for (int i = 1;i <= n;i++)
+  for (int i = 1;i <= m;i++)
     cin >> b[i];
+  sort(a + 1,a + n + 1);
+  sort(b + 1,b + m + 1);
   for (int i = 1;i <= n;i++)
-    for (int j = 1;j <= n;j++)
-      t[i][j] = a[i] * b[j] % m;
-  for (int i = 1;i <= n;i++)
-    for (int j = 1;j <= n;j++)
-      for (int k = 1;k <= n;k++)
-        if (c[i][j] == 0)
-          cout << c[i][k] << ' ';
-  return 0; 
+    pa[i] = pa[i - 1] + a[i];
+  for (int i = 1;i <= m;i++)
+    pb[i] = pb[i - 1] + b[i],
+    nd[i] = nd[i - 1] + (b[i] + k - 1) / k;
+  ll money = x + y * k;
+  int j = upper_bound(pa,pa + n + 1,money) - pa - 1;
+  ans = j;
+  for (int i = 1;i <= m;i++){
+    if (nd[i] > y) break;
+    while (j && pa[j] > money - pb[i]) j--;
+    ans = max(ans,i + j);
+  }
+  cout << ans;
+  return 0;
 }
